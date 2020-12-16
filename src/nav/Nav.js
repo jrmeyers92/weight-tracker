56385f58678de77b5063df38201fef87e3d9decc
Jake Meyers <jakejake92@yahoo.com>
2020-12-16 15:29:48 -0700

    app cleanup

---
+++ src/nav/Nav.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+const Nav = () => {
+
+    return (
+        <div>
+            <nav className="nav">
+                <ul className="nav__list">
+                    <li className='nav__item'>Input Weight</li>
+                    <li className='nav__item'>Dashboard</li>
+                    <li className='nav__item'>Goals</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export Default Nav;
